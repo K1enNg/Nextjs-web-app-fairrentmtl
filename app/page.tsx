233490a7b50c5dad07b12ba2ba5c8ecf6cf4645f
Kinkin <hiimkien2702@gmail.com>
2024-12-16 19:44:24 -0500

    fixed SplashScreen rendering

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,10 @@ import Hero from "./components/Hero";
 
 
 export default function Home() {
-  const pathname = usePathname();
-  const isHome = pathname === "/"
-  const [isLoading, setIsLoading] = useState(isHome);
 
-  useEffect(() => {
-    if (isLoading) return
-  }, [isLoading]) 
   return (
     <>
-      { isLoading && isHome ? (
-        <SplashScreen finishLoading={() => setIsLoading(false)}/>
-      ) : (
-        <Hero/>
-      )}
-
+      <Hero/>
     </>
   );
 }
